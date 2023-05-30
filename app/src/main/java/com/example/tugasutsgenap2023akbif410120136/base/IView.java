3fb945cb751f08ce9497ddd2551fca1ac72a08c1
AlifAziz36 <130490167+AlifAziz36@users.noreply.github.com>
2023-05-30 13:50:52 +0700

    IView.java

--- app/src/main/java/com/example/tugasutsgenap2023akbif410120136/base/IView.java
+++ app/src/main/java/com/example/tugasutsgenap2023akbif410120136/base/IView.java
@@ -1,6 +1,6 @@
 package com.example.tugasutsgenap2023akbif410120136.base;
 
-//04 June 2021, 10118737, Moch Rifqi Ramdhani, IF10
+//10120136, Alif Aziz Alviansyah, IF4
 public interface IView {
     void onAttachView();
     void onDetachView();
